dc70a35d6d009ecd32cc0fdb38e125c5851cb99b
tienonic <110650749+tienonic@users.noreply.github.com>
2026-03-19 19:04:30 -0700

    fix LaTeX rendering when options mix currency $ and math $...$

--- src/core/hooks/useLatex.ts
+++ src/core/hooks/useLatex.ts
@@ -3,8 +3,8 @@ import renderMathInElement from 'katex/contrib/auto-render';
 export function renderLatex(el: HTMLElement) {
   renderMathInElement(el, {
     delimiters: [
-      { left: '$$', right: '$$', display: true },
-      { left: '$', right: '$', display: false },
+      { left: '\\[', right: '\\]', display: true },
+      { left: '\\(', right: '\\)', display: false },
     ],
     throwOnError: false,
   });
